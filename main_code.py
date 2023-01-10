9dc8803db63a8cbc20842278a035ece24385fc6c
AslamTanjung <a.hanif.tanjung@gmail.com>
2023-01-10 22:51:06 +0100

    Read in ZIP file

--- main_code.py
+++ main_code.py
@@ -1,11 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import zipfile as zip
 
-print('peepee')
-import random
-n = random.random()
-print(n)
-
-3+3
-
+z = zip.ZipFile("Data/2qjral2ukjqiov77_csv.zip", "r")
+file = pd.read_csv(z.open(z.namelist()[0]))
