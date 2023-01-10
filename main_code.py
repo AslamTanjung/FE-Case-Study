9a3ee3a182f5c03591c1e86089ae51c43999b13c
AslamTanjung <a.hanif.tanjung@gmail.com>
2023-01-10 23:25:24 +0100

    Zip sluiten na openen

--- main_code.py
+++ main_code.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import zipfile as zip
-import matplotlib.pyplot as plt
 
 ######################################################################################
 ## FUNCTIONS #########################################################################
@@ -10,9 +9,9 @@ import matplotlib.pyplot as plt
 def read_zip(filename):
     z = zip.ZipFile(filename, "r")
     df = pd.read_csv(z.open(z.namelist()[0]))
+    z.close()
     return df
 
 ######################################################################################
 ## INLEZEN ###########################################################################
-z = zip.ZipFile("Data/2qjral2ukjqiov77_csv.zip", "r")
-df_2013 = pd.read_csv(z.open(z.namelist()[0]))
+df = read_zip("Data/2011.zip")
