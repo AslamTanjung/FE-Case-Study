80de270212cdefe273b3952d3db4c230173f85c1
AslamTanjung <a.hanif.tanjung@gmail.com>
2023-01-10 23:10:55 +0100

    Inlezen functie toegevoegd

--- main_code.py
+++ main_code.py
@@ -2,6 +2,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import zipfile as zip
+import matplotlib.pyplot as plt
 
+######################################################################################
+## FUNCTIONS #########################################################################
+
+def read_zip(filename):
+    z = zip.ZipFile(filename, "r")
+    df = pd.read_csv(z.open(z.namelist()[0]))
+    return df
+
+######################################################################################
+## INLEZEN ###########################################################################
 z = zip.ZipFile("Data/2qjral2ukjqiov77_csv.zip", "r")
-file = pd.read_csv(z.open(z.namelist()[0]))
+df_2013 = pd.read_csv(z.open(z.namelist()[0]))
