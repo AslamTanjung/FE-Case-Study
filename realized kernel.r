722dc464c8941c683f7fccabf24aa2c336f06631
AslamTanjung <a.hanif.tanjung@gmail.com>
2023-01-23 21:40:20 +0100

    Read in the timeseries variables

--- realized kernel.r
+++ realized kernel.r
@@ -12,14 +12,14 @@ data <- data %>%
 
 ts <- xts(data$PRICE, data$DATE_TIME)
 ## Realized kernel estimator
-kernel_cov <- rKernelCov(ts, cor = FALSE, alignBy = "seconds", alignPeriod = 1, makeReturns = TRUE, kernelType = "Parzen")
+kernel_cov <- rKernelCov(ts, cor = FALSE, alignBy = "minutes", alignPeriod = 1, makeReturns = TRUE, kernelType = "Parzen")
 
 ## Realized volatility
-cov <- rCov(ts, cor = FALSE, alignBy = "seconds", alignPeriod = 5, makeReturns = TRUE)
+cov <- rCov(ts, cor = FALSE, alignBy = "minutes", alignPeriod = 1, makeReturns = TRUE)
 
 ## Realized 
-plot(kernel_cov, col = 'green')
-lines(cov, col = 'red')
+plot(cov, col = 'red')
+lines(kernel_cov, col = 'blue')
 
 plot(kernel_cov-cov)
 
--- unnamed.R
+++ unnamed.R
@@ -5,6 +5,7 @@ library(xts)
 library(TTR)
 library(rugarch)
 library(ggplot2)
+library(TSstudio)
 
 data <- read.csv(unz("Data/Disney.zip", "Disney.csv"), header=T)
 ## Remove first row, because it is empty
@@ -30,14 +31,25 @@ ts_close <- xts(data_close$PRICE, data_close$DATE_TIME)
 ret_close <- momentum(100 * log(ts_close), n = 1)
 ret_oc <- xts(100 *(as.numeric(log(ts_open)) - as.numeric(log(ts_close))), index(ts_close))
 
-write.table(ret_close, file = "Close_to_close_log_returns.csv")
-write.table(ret_oc, file = "Open_to_close_log_returns.csv")
+write.zoo(ret_close, file = "Close_to_close_log_returns.csv", col.names = TRUE)
+write.zoo(ret_oc, file = "Open_to_close_log_returns.csv", col.names = TRUE)
+
+################################################################################
+################################################################################
+
+ret_close <- read.csv("Close_to_close_log_returns.csv", sep = " ")
+ret_close$Index <- as.POSIXct(ret_close$Index,format="%Y-%m-%d %H:%M:%S")
+ret_close <- xts(ret_close$V1, ret_close$Index)
+
+ret_oc <- read.csv("Open_to_close_log_returns.csv", sep = " ")
+ret_oc$Index <- as.POSIXct(ret_oc$Index,format="%Y-%m-%d %H:%M:%S")
+ret_oc <- xts(ret_oc$V1, ret_oc$Index)
 
 ## Normal returns
 # ret_close <- ROC(ts_close, n = 1)
 
-plot(index(ret_open), ret_open, type = "l", xlab = "Time", ylab = "Open Return")
-plot(index(ret_oc), ret_oc, type = "l", xlab = "Time", ylab = "Close Return")
+plot(index(ret_close), ret_close, type = "l", xlab = "Time", ylab = "Open to Close Log Return")
+plot(index(ret_oc), ret_oc, type = "l", xlab = "Time", ylab = "Close to Close Log Return")
 
 spec <- ugarchspec(mean.model = list(armaOrder = c(0, 0), include.mean = FALSE), variance.model = list(model = 'realGARCH', garchOrder = c(1, 1)), distribution.model = "norm")
 fit_ <- ugarchfit(spec, ret_close[2:length(ret_close)], solver = 'hybrid', realizedVol = kernel_cov[2:length(kernel_cov)])
