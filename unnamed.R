17c4304f7a980fe1a8628bcf812f233ec8aacea1
AslamTanjung <a.hanif.tanjung@gmail.com>
2023-01-23 20:02:00 +0100

    Realized garch

--- unnamed.R
+++ unnamed.R
@@ -26,11 +26,17 @@ ts_open <- xts(data_open$PRICE, data_open$DATE_TIME)
 ts_close <- xts(data_close$PRICE, data_close$DATE_TIME)
 
 ## Make returns
-ret_open <- ROC(ts_open, n = 1)
-ret_close <- ROC(ts_close, n = 1)
-
+## LOG return
+ret_open <- momentum(100 * log(ts_open), n = 1)
+#ret_close <- momentum(100 * log(ts_close), n = 1)
+ret_open_close <- 100 *(log(ts_open) - log(ts_close))
+## Normal returns
+# ret_open <- ROC(ts_open, n = 1)
+# ret_close <- ROC(ts_close, n = 1)
 
 plot(index(ret_open), ret_open, type = "l", xlab = "Time", ylab = "Open Return")
 plot(index(ret_close), ret_close, type = "l", xlab = "Time", ylab = "Close Return")
 
-##
+spec <- ugarchspec(mean.model = list(armaOrder = c(0, 0), include.mean = FALSE), variance.model = list(model = 'realGARCH', garchOrder = c(1, 1)), distribution.model = "norm")
+fit <- ugarchfit(spec, ret_close[2:length(ret_close)], solver = 'hybrid', realizedVol = kernel_cov[2:length(kernel_cov)])
+print(fit)
