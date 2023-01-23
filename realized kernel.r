099508b74e28711040dc069c6d88d18f072ba6e0
AslamTanjung <a.hanif.tanjung@gmail.com>
2023-01-23 20:13:48 +0100

    Log returns instead as csv

--- realized kernel.r
+++ realized kernel.r
@@ -12,7 +12,7 @@ data <- data %>%
 
 ts <- xts(data$PRICE, data$DATE_TIME)
 ## Realized kernel estimator
-kernel_cov <- rKernelCov(ts, cor = FALSE, alignBy = "seconds", alignPeriod = 5, makeReturns = TRUE, kernelType = "Parzen")
+kernel_cov <- rKernelCov(ts, cor = FALSE, alignBy = "seconds", alignPeriod = 1, makeReturns = TRUE, kernelType = "Parzen")
 
 ## Realized volatility
 cov <- rCov(ts, cor = FALSE, alignBy = "seconds", alignPeriod = 5, makeReturns = TRUE)
--- unnamed.R
+++ unnamed.R
@@ -27,16 +27,18 @@ ts_close <- xts(data_close$PRICE, data_close$DATE_TIME)
 
 ## Make returns
 ## LOG return
-ret_open <- momentum(100 * log(ts_open), n = 1)
-#ret_close <- momentum(100 * log(ts_close), n = 1)
-ret_open_close <- 100 *(log(ts_open) - log(ts_close))
+ret_close <- momentum(100 * log(ts_close), n = 1)
+ret_oc <- xts(100 *(as.numeric(log(ts_open)) - as.numeric(log(ts_close))), index(ts_close))
+
+write.table(ret_close, file = "Close_to_close_log_returns.csv")
+write.table(ret_oc, file = "Open_to_close_log_returns.csv")
+
 ## Normal returns
-# ret_open <- ROC(ts_open, n = 1)
 # ret_close <- ROC(ts_close, n = 1)
 
 plot(index(ret_open), ret_open, type = "l", xlab = "Time", ylab = "Open Return")
-plot(index(ret_close), ret_close, type = "l", xlab = "Time", ylab = "Close Return")
+plot(index(ret_oc), ret_oc, type = "l", xlab = "Time", ylab = "Close Return")
 
 spec <- ugarchspec(mean.model = list(armaOrder = c(0, 0), include.mean = FALSE), variance.model = list(model = 'realGARCH', garchOrder = c(1, 1)), distribution.model = "norm")
-fit <- ugarchfit(spec, ret_close[2:length(ret_close)], solver = 'hybrid', realizedVol = kernel_cov[2:length(kernel_cov)])
+fit_ <- ugarchfit(spec, ret_close[2:length(ret_close)], solver = 'hybrid', realizedVol = kernel_cov[2:length(kernel_cov)])
 print(fit)
