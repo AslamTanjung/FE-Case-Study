e803c742e4eb0d9d627992a5fdfa2463ece5aaf9
AslamTanjung <a.hanif.tanjung@gmail.com>
2023-01-28 01:15:30 +0100

    nieuwe GAS models

--- GAS.R
+++ GAS.R
@@ -25,21 +25,12 @@ ret <- xts(ret$V1, ret$Index)
 
 n_test <- 252 * 2
 for (name in c(
-  "norm",
-  "snorm",
   "std",
   "sstd",
   "ast",
-  "ast1",
-  "ald",
-  "poi",
-  "gamma",
-  "exp",
-  "beta",
-  "negbin",
-  "skellam"
+  "ald"
 )){
-  for(scaling in c("Identity", "Inv", "InvSqrt")) {
+  for(scaling in c("Inv")) {
   print(paste(name, scaling))
   
   spec <- UniGASSpec(
@@ -82,6 +73,65 @@ for (name in c(
   )
   }
 }
+
+
+## Close-to-close
+ret_cc <-
+  read.csv(file = "Returns & RV/Close_to_close_log_returns.csv", sep = " ")
+ret_cc$Index <- as.POSIXct(ret_cc$Index, format = "%Y-%m-%d %H:%M:%S")
+ret_cc <- xts(ret_cc$V1, ret_cc$Index)
+
+n_test <- 252 * 2
+for (name in c(
+  "std",
+  "sstd",
+  "ast",
+  "ald"
+)){
+  for(scaling in c("Inv")) {
+    print(paste(name, scaling))
+    
+    spec <- UniGASSpec(
+      Dist = name,
+      ScalingType = scaling,
+      GASPar = list(
+        location = TRUE,
+        scale = TRUE,
+        skewness = TRUE,
+        shape = TRUE,
+        shape2 = TRUE
+      )
+    )
+    
+    # fit <-
+    #    UniGASFit(spec, ret, fn.optimizer = fn.optim, Compute.SE = TRUE)
+    
+    rol_for <- UniGASRoll(
+      ret_cc,
+      spec,
+      ForecastLength = n_test,
+      RefitEvery = 1,
+      RefitWindow = "moving",
+      cluster = cluster,
+      Compute.SE = FALSE
+    )
+    true_value <- tail(kernel_cov, n_test)
+    
+    forc_gas <- sqrt(rol_for@Forecast$Moments[, 2])
+    write.csv(
+      forc_gas,
+      file = paste0("Forecasts/Close_to_close_GAS_", name, "_", scaling, "_Forecast.csv"),
+      row.names = FALSE
+    )
+    MAE_gas <- as.numeric(abs(forc_gas - true_value))
+    write.csv(
+      MAE_gas,
+      file = paste0("MAE/Close_to_close_GAS_", name, "_", scaling, "_MAE.csv"),
+      row.names = FALSE
+    )
+  }
+}
+################################################################################
 
 MAE_roc <- read.csv("MAE/Open_to_close_RealGARCH_MAE.csv")$x
 MAE_oc <- read.csv("MAE/Open_to_close_GARCH_MAE.csv")$x
