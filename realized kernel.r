1ff557278f802bcfd17f54dbb251643f3736691f
Svenn07 <svenjurn@gmail.com>
2023-01-29 12:01:32 +0100

    andere realized Kernel
    see file RV_kernel2

--- realized kernel.r
+++ realized kernel.r
@@ -15,9 +15,17 @@ ts <- xts(data$PRICE, data$DATE_TIME)
 #make returns
 ts1 <- momentum(100 * log(ts), n = 1)
 ts1 <- ts1[-1]
+
+ts2 <- 100*makeReturns(ts)
+ts2 <- ts2[-1]
 ## Realized kernel estimator
 kernel_cov <- rKernelCov(ts, cor = FALSE, alignBy = "minutes", alignPeriod = 1, makeReturns = TRUE, kernelType = "Parzen")
-write.zoo(kernel_cov*10000, file = "Returns & RVRV_kernel.csv", col.names = TRUE)
+write.zoo(kernel_cov*10000, file = "Returns & RV/RV_kernel.csv", col.names = TRUE)
+
+kernel_cov2 <- rKernelCov(ts2, cor = FALSE, alignBy = "minutes", alignPeriod = 1, makeReturns = FALSE, kernelType = "Parzen")
+write.zoo(kernel_cov2, file = "Returns & RV/RV_kernel2.csv", col.names = TRUE)
+
+
 ## Realized volatility
 cov <- rCov(ts, cor = FALSE, alignBy = "minutes", alignPeriod = 1, makeReturns = TRUE)
 write.zoo(cov*10000, file = "Returns & RV/RV.csv", col.names = TRUE)
